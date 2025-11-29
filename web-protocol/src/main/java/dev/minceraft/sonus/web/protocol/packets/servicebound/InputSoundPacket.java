83547b39c7a1e0f9f2891a17efb87b0bf98586ea
booky <booky@booky.dev>
2025-11-29 05:32:21 +0100

    Increase max web input frame size

--- web-protocol/src/main/java/dev/minceraft/sonus/web/protocol/packets/servicebound/InputSoundPacket.java
+++ web-protocol/src/main/java/dev/minceraft/sonus/web/protocol/packets/servicebound/InputSoundPacket.java
@@ -1,7 +1,6 @@
 package dev.minceraft.sonus.web.protocol.packets.servicebound;
 // Created by booky10 in Sonus (20:33 28.11.2025)
 
-import dev.minceraft.sonus.common.SonusConstants;
 import dev.minceraft.sonus.common.audio.SonusAudio;
 import dev.minceraft.sonus.common.protocol.util.DataTypeUtil;
 import dev.minceraft.sonus.web.protocol.WsPacketContext;
@@ -14,8 +13,7 @@ import org.jspecify.annotations.NullMarked;
 @NullMarked
 public class InputSoundPacket extends WebSocketPacket {
 
-    private static final int MAX_OPUS_BITRATE = 64_000; // according to https://wiki.xiph.org/Opus_Recommended_Settings#Bandwidth_Transition_Thresholds
-    private static final int MAX_OPUS_BYTES_PER_FRAME = MAX_OPUS_BITRATE / Byte.SIZE / SonusConstants.FRAMES_PER_SECOND;
+    private static final int MAX_OPUS_BYTES_PER_FRAME = 400; // arbitrary limit
 
     private SonusAudio.@MonotonicNonNull Opus audio;
     private boolean noiseReduction;
