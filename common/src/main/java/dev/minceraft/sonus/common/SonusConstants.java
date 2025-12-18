d6ae28507055e4faeb83a51ea9f90eb0ac011817
pianoman911 <kontakt@finndohrmann.de>
2025-12-19 00:30:35 +0100

    Fixed permission nodes

--- common/src/main/java/dev/minceraft/sonus/common/SonusConstants.java
+++ common/src/main/java/dev/minceraft/sonus/common/SonusConstants.java
@@ -16,11 +16,11 @@ public final class SonusConstants {
     public static final int CHANNELS = 1; // Mono
     public static final int CURRENT_VERSION = 0;
 
-    public static final String PERMISSION_VOICE_SPEAK = "sous.voice.speak";
-    public static final String PERMISSION_VOICE_LISTEN = "sous.voice.listen";
-    public static final String PERMISSION_CONNECT_SVC = "sous.connect.svc";
-    public static final String PERMISSION_CONNECT_PLASMO = "sous.connect.plasmo";
-    public static final String PERMISSION_BYPASS_GROUP_PASSWORD = "sous.bypass.group-password";
+    public static final String PERMISSION_VOICE_SPEAK = "sonus.voice.speak";
+    public static final String PERMISSION_VOICE_LISTEN = "sonus.voice.listen";
+    public static final String PERMISSION_CONNECT_SVC = "sonus.connect.svc";
+    public static final String PERMISSION_CONNECT_PLASMO = "sonus.connect.plasmo";
+    public static final String PERMISSION_BYPASS_GROUP_PASSWORD = "sonus.bypass.group-password";
 
     private SonusConstants() {
     }
